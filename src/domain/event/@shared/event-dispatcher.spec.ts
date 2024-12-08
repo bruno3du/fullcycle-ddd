8a5eaef7e38be3a92fe4c9fe55eeb806fd2838b0
Bruno <bruno.139737@gmail.com>
2024-12-07 22:55:59 -0300

    learning domain events

---
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,69 @@
+import SendEmailWhenProductIsCreatedHandler from "../handler/send-email-when-product-is-created.handler";
+import ProductCreatedEvent from "../product/product-created.event";
+import EventDispatcher from "./event-dispatcher";
+
+describe("Domain events tests", () => {
+  it("should be able to register an event handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+    ).toBeDefined();
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(
+      1
+    );
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toBe(
+      eventHandler
+    );
+  });
+
+  it("should be able to unregister an event handler", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(
+      1
+    );
+
+    eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
+
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+    ).toBeDefined();
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(
+      0
+    );
+  });
+
+  it("should be able to unregister all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+    eventDispatcher.unregisterAll();
+
+    expect(
+      eventDispatcher.getEventHandlers["ProductCreatedEvent"]
+    ).toBeUndefined();
+  });
+
+  it("should be able to notify all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    const productCreatedEvent = new ProductCreatedEvent({
+      name: "Product 1",
+      description: "Product 1 description",
+      price: 10,
+    });
+
+    eventDispatcher.notify(productCreatedEvent);
+    expect(spyEventHandler).toHaveBeenCalledTimes(1);
+    expect(eventHandler.handle).toHaveBeenCalledWith(productCreatedEvent);
+  });
+});
---
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -0,0 +1,38 @@
+import { EventDispatcherInterface } from "./event-dispatcher.interface";
+import { EventHandlerInterface } from "./event-handler.interface";
+import { EventInterface } from "./event.interface";
+
+interface EventHandlers {
+  [eventName: string]: EventHandlerInterface[];
+}
+
+export default class EventDispatcher implements EventDispatcherInterface {
+  private eventHandlers: EventHandlers = {};
+
+  get getEventHandlers(): EventHandlers {
+    return this.eventHandlers;
+  }
+  notify(event: EventInterface): void {
+    if (this.eventHandlers[event.constructor.name]) {
+      this.eventHandlers[event.constructor.name].forEach((eventHandler) => {
+        eventHandler.handle(event);
+      });
+    }
+  }
+  register(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+    this.eventHandlers[eventName].push(eventHandler);
+  }
+  unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (eventName in this.eventHandlers) {
+      this.eventHandlers[eventName] = this.eventHandlers[eventName].filter(
+        (event) => event !== eventHandler
+      );
+    }
+  }
+  unregisterAll(): void {
+    this.eventHandlers = {};
+  }
+}
